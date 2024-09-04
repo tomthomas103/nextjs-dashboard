6b2fb6fc4d2b3ea886f59ec445daf74c756d5aff
Tom Thomas <tomthomas@live.in>
2024-09-04 21:02:05 +0530

    depo bug fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    output: 'export',
-  // Optional configurations
-  trailingSlash: true,
-  distDir: 'dist',
+    
 };
 
 export default nextConfig;
